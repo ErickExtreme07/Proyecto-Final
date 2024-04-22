715839d98dfe2f9b8b07b5767d79a022cfdfcdeb
Luisellisc <157877916+Luisellisc@users.noreply.github.com>
2024-04-22 17:13:49 -0500

    Update gato.java

--- src/animales/gato.java
+++ src/animales/gato.java
@@ -1,4 +1,28 @@
 package animales;
 
-public class gato extends animales{
+import acciones.Animal;
+
+public class gato extends Animal{
+    private String especie;
+
+    public gato(String nombre, int edad, String especie, String estadoSalud) {
+        super(nombre, edad, especie, estadoSalud);
+        this.especie = especie;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public void alimentar() {
+        System.out.println(getNombre() + " El gato ha sido alimentado.");
+    }
+
+    public void dormir() {
+        System.out.println(getNombre() + " El gato está durmiendo.");
+    }
+
+    public void jugar() {
+        System.out.println(getNombre() + " El gato está jugando con su juguete.");
+    }
 }
